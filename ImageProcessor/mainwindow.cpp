81fe066b8c1a475d94c703fb5fa39c021093d5a1
RiccardoZappa <rzappa00@gmail.com>
2024-11-12 23:13:19 +0100

    started the implementation with a loadImage pushbutton

--- ImageProcessor/mainwindow.cpp
+++ ImageProcessor/mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->LoadImage, &QPushButton::clicked, this, &MainWindow::onLoadImageClicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onLoadImageClicked()
+{
+    // This is the backend function handling the button click
+    QMessageBox::information(this, "Button Clicked", "The push button was clicked!");
 }
